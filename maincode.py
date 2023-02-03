541ef1768e1e45da45f7b45a265948c1400bc0d1
jvtcoding <jacobvontress@gmail.com>
2023-02-03 09:49:07 -0600

    trying to break the window

--- maincode.py
+++ maincode.py
@@ -49,10 +49,10 @@ class Trash(Frame):
             #           between the trash and the lid.".format(int(percent), int(insideDist)))
             text = Label(window, width = 400, height = 200, text=myText, font = ("Playbill", 16))
             text.pack(side = TOP)
-            button = Button(window, width = 400, height = 200, text = "Exit", font = ("Playbill", 16), command = self.close())
-            button.pack(side = BOTTOM)
+            #button = Button(window, width = 400, height = 200, text = "Exit", font = ("Playbill", 16), command = quit)
+            #button.pack(side = BOTTOM)
             if (GPIO.input(button) == GPIO.HIGH):
-                window.destroy()
+                break
             window.after(1000, window.quit)
             window.mainloop()
             self.frontScan()
