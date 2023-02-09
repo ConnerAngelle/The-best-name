9aa82c922f11bd79f90784239fb6eebc27e8e0c5
jvtcoding <jacobvontress@gmail.com>
2023-02-09 12:48:26 -0600

    fix button layout

--- maincode.py
+++ maincode.py
@@ -47,7 +47,7 @@ class Trash(Frame):
         openButton.destroy()
         closeButton = Button(window, text = "Close",\
                              command = lambda: self.close(closeButton, text))
-        closeButton.pack(side = BOTTOM, anchor = "e")
+        closeButton.pack(side = BOTTOM, anchor = "e", padx = 100)
         servo.value = 0.3
         while(ocPressed == 0):
             window.update()
@@ -61,7 +61,7 @@ class Trash(Frame):
         closeButton.destroy()
         openButton = Button(window, text = "Open",\
                             command = lambda: self.stayOpen(openButton, text))
-        openButton.pack(side = BOTTOM, anchor = "e")
+        openButton.pack(side = BOTTOM, anchor = "e", padx = 100)
         sleep(1.5)
         text.destroy()
         percent = self.singleScan()
@@ -77,12 +77,12 @@ class Trash(Frame):
         buttonPressed = c
         exitButton = Button(window, text = "Exit",\
                             command = lambda: self.Quit())
-        exitButton.pack(side = BOTTOM, anchor = "w")
+        exitButton.pack(side = BOTTOM, anchor = "w", padx = 100)
 
         #button to keep the lid open
         openButton = Button(window, text = "Open",\
                             command = lambda: self.stayOpen(openButton, text))
-        openButton.pack(side = BOTTOM, anchor = "e")
+        openButton.pack(side = BOTTOM, anchor = "e", padx = 100)
 
         # set the servo to its starting position
         servo.min()
